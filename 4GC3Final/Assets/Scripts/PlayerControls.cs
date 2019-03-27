124a53c6f4b9c43b75af140ca7dc32e16fb31042
coitn <nathan_coit@hotmail.com>
2019-03-27 17:02:15 -0400

    Fixed the issue where players angular velocity was not being reset. Added functionality for loading different characters. Fixed an issue where player can move during ready screen.

--- 4GC3Final/Assets/Scripts/PlayerControls.cs
+++ 4GC3Final/Assets/Scripts/PlayerControls.cs
@@ -59,6 +59,7 @@ public class PlayerControls : MonoBehaviour
             {
                 // Jump
                 rigidbody.velocity = new Vector3(0, 0, 0);
+                rigidbody.angularVelocity = new Vector3(0, 0, 0);
                 rigidbody.AddForce(new Vector3(0, JumpForce, 0));
                 CurrentAction = ActionType.Jumping;
             }
@@ -75,11 +76,12 @@ public class PlayerControls : MonoBehaviour
         else
         {
             ActionTimeout--;
-            if(ActionTimeout == 0 && CurrentAction != ActionType.None)
+            if(ActionTimeout == 0 && CurrentAction != ActionType.None && CurrentAction != ActionType.Dead)
             {
                 if (CurrentAction == ActionType.Shoving)
                 {
                     rigidbody.velocity = new Vector3(0, 0, 0);
+                    rigidbody.angularVelocity = new Vector3(0, 0, 0);
                 }
                 CurrentAction = ActionType.None;
             }
@@ -121,6 +123,7 @@ public class PlayerControls : MonoBehaviour
     public void GetShoved(float ShoveTime, float ShoveForce, Vector3 ShoveVector)
     {
         rigidbody.velocity = new Vector3(0, 0, 0);
+        rigidbody.angularVelocity = new Vector3(0, 0, 0);
         rigidbody.AddForce(ShoveVector * ShoveForce);
         CurrentAction = ActionType.BeingShoved;
         ActionTimeout = (int)ShoveTime;
